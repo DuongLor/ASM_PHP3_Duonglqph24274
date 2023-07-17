b8923a828f7862a7bce13aa7a32db1679af4a59a
Duonglor <quyduong1201@gmail.com>
2023-07-17 22:44:37 +0700

    first commit

---
+++ database/migrations/2023_07_17_092651_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+						$table->unsignedBigInteger('hotel_id');
+						$table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade');
+						$table->string('name');
+						$table->unsignedBigInteger('type_id');
+						$table->foreign('type_id')->references('id')->on('room_types')->onDelete('cascade');
+						$table->decimal('price', 10, 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
